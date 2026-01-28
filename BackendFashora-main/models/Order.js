dce53b88de5e45d3bf77acc9d4442070e91f544f
Your Name <your.email@example.com>
2026-01-28 14:19:32 +0545

    ord

--- BackendFashora-main/models/Order.js
+++ BackendFashora-main/models/Order.js
@@ -1,44 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const orderSchema = new mongoose.Schema({
-//   fullName: {
-//     type: String,
-//     required: true
-//   },
-//   address: {
-//     type: String,
-//     required: true
-//   },
-//   phone: {
-//     type: String,
-//     required: true
-//   },
-//   paymentMethod: {
-//     type: String,
-//     enum: ['cod', 'esewa'],
-//     default: 'cod'
-//   },
-//   items: [
-//     {
-//       productId: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: "Product",
-//         required: true
-//       },
-//       quantity: {
-//         type: Number,
-//         required: true,
-//         min: 1
-//       }
-//     }
-//   ],
-//   createdAt: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
-
-// module.exports = mongoose.model("Order", orderSchema);
 
 
 const mongoose = require("mongoose");
