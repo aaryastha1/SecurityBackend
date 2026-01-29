a4a1581aef80c527a764a57e1dd3e9f697514cf7
Your Name <your.email@example.com>
2026-01-29 14:27:34 +0545

    up

--- BackendFashora-main/routers/admin/OrderRoute.js
+++ BackendFashora-main/routers/admin/OrderRoute.js
@@ -1,3 +1,50 @@
+// const express = require("express");
+// const router = express.Router();
+// const Order = require("../../models/Order");
+
+// // POST /api/orders/place
+// router.post("/place", async (req, res) => {
+//   try {
+//     const { fullName, address, phone, paymentMethod, items } = req.body;
+
+//     const newOrder = new Order({
+//       fullName,
+//       address,
+//       phone,
+//       paymentMethod,
+//       items
+//     });
+
+//     const savedOrder = await newOrder.save();
+//     res.status(201).json(savedOrder);
+//   } catch (err) {
+//     console.error("Error placing order:", err);
+//     res.status(400).json({ success: false, message: err.message });
+//   }
+
+
+// });
+
+
+
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const orders = await Order.find()
+//       .sort({ createdAt: -1 })
+//       .populate("items.productId", "name image price");
+
+//     res.json(orders); // ✅ correct: sends an array directly
+//   } catch (err) {
+//     console.error("Error fetching orders:", err);
+//     res.status(500).json({ success: false, message: "Failed to fetch orders" });
+//   }
+// });
+
+
+
+// module.exports = router;
+
 
 
 const express = require("express");
