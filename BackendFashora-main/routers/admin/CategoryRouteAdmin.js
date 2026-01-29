9b0fc6a4558c36218135976ad4d7748d5b9e564c
Your Name <your.email@example.com>
2026-01-29 14:25:55 +0545

    up

--- BackendFashora-main/routers/admin/CategoryRouteAdmin.js
+++ BackendFashora-main/routers/admin/CategoryRouteAdmin.js
@@ -1,3 +1,23 @@
+// const express = require('express');
+// const router = express.Router();
+// const categoryController = require('../../controllers/admin/categorymanagement');
+// const upload = require('../../middlewares/fileupload'); // multer setup
+
+// router.post(
+//     '/', 
+//     upload.single("image"),
+//     // req.file, req.files from next function
+//     // get image, file metadata
+//     categoryController.createCategory
+// );
+// router.get('/', categoryController.getAllCategories);
+// router.get('/:id', categoryController.getCategoryById);
+// router.put('/:id', 
+//     upload.single("image"),
+//     categoryController.updateCategory);
+// router.delete('/:id', categoryController.deleteCategory);
+
+// module.exports = router;
 
 
 
